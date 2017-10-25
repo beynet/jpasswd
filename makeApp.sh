ce23489aab4ceea285d89ff416c3424c3bc08518
Yannick Beynet <beynet@fresnes.info>
2017-10-25 16:32:44 +0200

    next version 1.0.4

--- makeApp.sh
+++ makeApp.sh
@@ -2,7 +2,7 @@
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-9/Contents/Home/
 
 mvn clean package -Dmaven.test.skip=true && \
-$JAVA_HOME/bin/javapackager -deploy -native image -srcdir target -srcfiles jpasswd-1.0.3-jar-with-dependencies.jar -appclass org.beynet.gui.Main -name jpasswd -outdir ./target/app -outfile jpasswd && \
+$JAVA_HOME/bin/javapackager -deploy -native image -srcdir target -srcfiles jpasswd-1.0.4-jar-with-dependencies.jar -appclass org.beynet.gui.Main -name jpasswd -outdir ./target/app -outfile jpasswd && \
 cd target/app/ && \
 hdiutil create -size 300m -fs HFS+ -volname "jpasswd" jpasswd-w.dmg && \
 DEVS=$(hdiutil attach jpasswd-w.dmg | cut -f 1) && \
