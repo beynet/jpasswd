2da6349fb4199c92ceb82176db9be715f7fea4a4
Yannick Beynet <beynet@fresnes.info>
2020-02-22 23:41:58 +0100

    start migrate to drive v3

--- src/test/java/org/beynet/gui/GoogleDriveAuthentTest.java
+++ src/test/java/org/beynet/gui/GoogleDriveAuthentTest.java
@@ -1,10 +1,16 @@
 package org.beynet.gui;
 
+import org.beynet.model.store.RootTest;
+import org.beynet.sync.googledrive.ListFilesReader;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ArrayNode;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,17 +18,34 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * Created by beynet on 23/10/14.
  */
-public class GoogleDriveAuthentTest {
+public class GoogleDriveAuthentTest extends RootTest {
 
     @Test
     public void testCodeRetrieval() {
-        String expected ="this/istheexpectedcode";
-        String uri = "http://localhost?code="+expected;
-        assertThat(GoogleDriveAuthent.getCodeFromURL(uri), is(expected));
-        uri = "http://localhost?val1=val2&code="+expected+"&val3=val4&val5=val6";
+        String expected ="4/wwHZ1YxHxR0T5fvyXjPxPKWn7pQRpSOXreI_ukOzTXY8yzecJ3IEs7DxrW66GDt-_VpyTAVN90Jbhj9bVIfXCYg";
+        String uri = "http://localhost/?code=4%2FwwHZ1YxHxR0T5fvyXjPxPKWn7pQRpSOXreI_ukOzTXY8yzecJ3IEs7DxrW66GDt-_VpyTAVN90Jbhj9bVIfXCYg&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.file";
         assertThat(GoogleDriveAuthent.getCodeFromURL(uri), is(expected));
     }
 
+    String listFilesResultV3 = "{\n" +
+            " \"kind\": \"drive#fileList\",\n" +
+            " \"incompleteSearch\": false,\n" +
+            " \"files\": [\n" +
+            "  {\n" +
+            "   \"kind\": \"drive#file\",\n" +
+            "   \"id\": \"1gxxEdQ9uHgvc3UKxKJoD8tMmAPhxhhuj\",\n" +
+            "   \"name\": \"jptest.dat\",\n" +
+            "   \"mimeType\": \"application/dat\"\n" +
+            "  }\n" +
+            " ]\n" +
+            "}\n";
+    @Test
+    public void readFileId() throws Exception {
+        Optional<JsonNode> fileFromListV3 = ListFilesReader.getFileFromListV3(listFilesResultV3);
+        assertThat(fileFromListV3.isPresent(),is(true));
+        JsonNode node = fileFromListV3.get();
+        assertThat(node.get("id").getTextValue(),is("1gxxEdQ9uHgvc3UKxKJoD8tMmAPhxhhuj"));
+    }
 
 
 }
