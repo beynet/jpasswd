f5c43fc1c99fade0a6163300ec30c23f84a4e9f0
beynet <beynet@fresnes.info>
2020-08-26 14:54:48 +0200

    remove erronous passwd

--- src/main/java/org/beynet/gui/GoogleDriveAuthent.java
+++ src/main/java/org/beynet/gui/GoogleDriveAuthent.java
@@ -23,8 +23,6 @@ public class GoogleDriveAuthent extends Dialog {
         final WebView browser = new WebView();
         final WebEngine webEngine = browser.getEngine();
 
-        webEngine.setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.83 Safari/537.1");
-
         webEngine.locationProperty().addListener((observable, oldValue, newValue) -> {
             System.out.println("old="+oldValue+" new="+newValue);
             if (newValue!=null && newValue.startsWith("http://localhost")) {
