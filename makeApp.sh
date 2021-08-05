2d7e5d53ee1abfd31fb8cbd21ae9ac85e95a976c
Yannick Beynet <beynet@fresnes.info>
2021-08-05 10:35:00 +0200

    simplify password generator

--- makeApp.sh
+++ makeApp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-14.0.1.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu11.45.27-ca-jdk11.0.10-macosx_x64/zulu-11.jdk/Contents/Home
+export JPACKAGE=/Library/Java/JavaVirtualMachines/jdk-14.0.1.jdk/Contents/Home/bin/jpackage
+
 
 mvn clean package -Dmaven.test.skip=true && \
-$JAVA_HOME/bin/jpackage --runtime-image $JAVA_HOME  -n jpasswd --main-class org.beynet.gui.Main --main-jar jpasswd-1.0.5-SNAPSHOT-jar-with-dependencies.jar --input ./target
+$JPACKAGE --runtime-image $JAVA_HOME  -n jpasswd --main-class org.beynet.gui.Main --main-jar jpasswd-1.0.8-SNAPSHOT-jar-with-dependencies.jar --input ./target
